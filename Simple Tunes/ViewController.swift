e020629b5edcbe77635b370f184dd794d18ecd30
Mwangi Muriuki <muriukimwangi.mm@gmail.com>
2020-02-19 13:09:05 +0300

    Next Commit

--- Simple Tunes/ViewController.swift
+++ Simple Tunes/ViewController.swift
@@ -7,14 +7,45 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, AVAudioPlayerDelegate {
+    
+    let noteArray = ["note1", "note2", "note3", "note4", "note5", "note6", "note7"]
+    
+    var audioPlayer: AVAudioPlayer!
+    var selectedButtonTag: String = ""
+        
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func keyPressed(_ sender: UIButton) {
+      
+       selectedButtonTag = noteArray[sender.tag - 1]
+        
+        print(selectedButtonTag)
+        
+       playSound()
+        
+    }
+    
+    func playSound(){
+        
+               let soundURL = Bundle.main.url(forResource: selectedButtonTag, withExtension: "wav")
+               
+               do{
+                   audioPlayer = try AVAudioPlayer(contentsOf: soundURL!)
+               }
+               catch{
+                   print(error)
+               }
+               
+               audioPlayer.play()
+        
+    }
+    
+    
 }
 
